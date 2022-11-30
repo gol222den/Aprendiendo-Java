c16aa7d30cf4e598030d4307f6b272912ee6f44f
gol222den <fmonti@alumno.huergo.edu.ar>
2022-11-30 01:55:39 -0300

    primer commit

---
+++ DOCUMENTACION/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+/**
+ * clase principal punto de entrada aplicacion
+ * @author
+ */
+public class Main {
+
+    /**
+     *
+     * funcion principal punto de entrada a la aplicacion
+     * @param args argumentos a recibir
+     * @since 1.0
+     */
+
+    public static void main(String[] args) {
+
+
+        /* This is a coment */
+
+        /*
+        1. Abrir archivos
+        2. Leer archivos
+        3. Cerrar archivos
+         */
+
+        // Imprimir texto
+        System.out.println("Hola Mundo"); // imprimir texto
+
+
+    }
+}
